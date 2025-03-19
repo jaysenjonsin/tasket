b824e25488edfbf0ce35f8bd9046ca975b947165
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-03-19 11:53:39 -0700

    implement logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useCurrent } from '@/features/auth/api/use-current';
 import { useRouter } from 'next/navigation';
 import { useLogout } from '@/features/auth/api/use-logout';
 import { Button } from '@/components/ui/button';
+import { UserButton } from '../features/auth/components/user-button';
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
@@ -15,8 +16,7 @@ export default function Home() {
   }, [data]);
   return (
     <div>
-      visible to authorized users
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
