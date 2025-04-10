8be527859edceb95fea14cb7f8d293d3062c7c9f
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-04-09 17:28:23 -0700

    create members api route

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,10 +1,10 @@
 import { getCurrent } from '@/features/auth/queries';
 import { redirect } from 'next/navigation';
 
-const WorkspaceIdPage = async ({ params }: any) => {
+const WorkspaceIdPage = async () => {
   const user = await getCurrent();
   if (!user) redirect('/sign-in');
-  return <div>Workspace Id: {params.workspaceId}</div>;
+  return <div>Workspace Id</div>;
 };
 
 export default WorkspaceIdPage;
--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -4,10 +4,13 @@ import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 import auth from '@/features/auth/server/route';
 import workspaces from '@/features/workspaces/server/route';
-
+import members from '@/features/members/server/route';
 const app = new Hono().basePath('/api');
 
-const routes = app.route('/auth', auth).route('/workspaces', workspaces);
+const routes = app
+  .route('/auth', auth)
+  .route('/workspaces', workspaces)
+  .route('/members', members);
 
 export const GET = handle(app); //handle adapts the hono app to work with vercel edge runtime, makes sure all routing and middleware work correctly. hono and vercel have diff ways of handling HTTP requests and handle bridges the gap
 export const POST = handle(app);
