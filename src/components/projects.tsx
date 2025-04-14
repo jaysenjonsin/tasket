ab48268c9ec4d0b346cb7affb543d9bcdfa8215d
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-04-14 12:37:37 -0700

    create create project api endpoint

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -5,8 +5,10 @@ import { useGetProjects } from '@/features/projects/api/use-get-projects';
 import { useWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { cn } from '../lib/utils';
 
 export const Projects = () => {
+  const projectId = null;
   const workspaceId = useWorkspaceId();
   const { data } = useGetProjects({
     workspaceId,
@@ -24,7 +26,18 @@ export const Projects = () => {
       {data?.documents.map((project) => {
         const href = `/workspaces/${workspaceId}/projects/${project.$id}`;
         const isActive = pathname === href;
-        return <Link href={href} key={project.$id}></Link>;
+        return (
+          <Link href={href} key={project.$id}>
+            <div
+              className={cn(
+                'flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500',
+                isActive && 'bg-white shadow-sm hover:opacity-100 text-primary'
+              )}
+            >
+              <span className='truncate'>{project.name}</span>
+            </div>
+          </Link>
+        );
       })}
     </div>
   );
--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -3,40 +3,98 @@ import { sessionMiddleware } from '@/lib/session-middleware';
 import { zValidator } from '@hono/zod-validator';
 import { z } from 'zod';
 import { getMember } from '@/features/members/utils';
-import { PROJECTS_ID } from '@/config';
+import { IMAGES_BUCKET_ID, PROJECTS_ID } from '@/config';
 import { DATABASE_ID } from '@/config';
-import { Query } from 'node-appwrite';
+import { ID, Query } from 'node-appwrite';
+import { createProjectSchema } from '../schema';
 
-const app = new Hono().get(
-  '/',
-  sessionMiddleware,
-  zValidator('query', z.object({ workspaceId: z.string() })),
-  async (c) => {
-    const user = c.get('user');
-    const databases = c.get('databases');
+const app = new Hono()
+  .get(
+    '/',
+    sessionMiddleware,
+    zValidator('query', z.object({ workspaceId: z.string() })),
+    async (c) => {
+      const user = c.get('user');
+      const databases = c.get('databases');
 
-    const { workspaceId } = c.req.valid('query');
+      const { workspaceId } = c.req.valid('query');
 
-    if (!workspaceId) {
-      return c.json({ error: 'Missing workspaceId' }, 400);
+      if (!workspaceId) {
+        return c.json({ error: 'Missing workspaceId' }, 400);
+      }
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+      if (!member) {
+        return c.json({ error: 'Not Authorized' }, 401);
+      }
+
+      const projects = await databases.listDocuments(DATABASE_ID, PROJECTS_ID, [
+        Query.equal('workspaceId', workspaceId),
+        Query.orderAsc('$createdAt'),
+      ]);
+
+      return c.json({ data: projects });
     }
+  )
+  .post(
+    '/',
+    sessionMiddleware,
+    zValidator('form', createProjectSchema),
+    async (c) => {
+      const databases = c.get('databases');
+      const storage = c.get('storage');
+      const user = c.get('user');
+      const { name, image, workspaceId } = c.req.valid('form');
 
-    const member = await getMember({
-      databases,
-      workspaceId,
-      userId: user.$id,
-    });
-    if (!member) {
-      return c.json({ error: 'Not Authorized' }, 401);
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json({ error: 'Not Authorized' }, 401);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+
+        //images are binary data, ArrayBuffer is a representation of binary data that can be handled in JS
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        //convert the arrayBuffer to a base64 string
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString('base64')}`;
+      }
+
+      const project = await databases.createDocument(
+        DATABASE_ID,
+        PROJECTS_ID,
+        ID.unique(),
+        {
+          name,
+          userId: user.$id,
+          imageUrl: uploadedImageUrl,
+          workspaceId,
+        }
+      );
+
+      return c.json({ data: project });
     }
-
-    const projects = await databases.listDocuments(DATABASE_ID, PROJECTS_ID, [
-      Query.equal('workspaceId', workspaceId),
-      Query.orderAsc('$createdAt'),
-    ]);
-
-    return c.json({ data: projects });
-  }
-);
+  );
 
 export default app;
