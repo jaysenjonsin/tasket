059080cb45b8c208d3211c598dd14a127f6d8b68
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-04-16 10:30:32 -0700

    add error and loading to routes

--- src/app/(auth)/error.tsx
+++ src/app/(auth)/error.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { AlertTriangle } from 'lucide-react';
-import { Button } from '../components/ui/button';
+import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 const ErrorPage = () => {
