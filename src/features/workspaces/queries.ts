e896daa4b32f1ee96357ba236231db2e9c1ae6e6
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-04-16 10:43:32 -0700

    refactor query error handling

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -4,65 +4,56 @@ import { getMember } from '@/features/members/utils';
 import { Workspace } from './types';
 import { createSessionClient } from '@/lib/appwrite';
 export const getWorkspaces = async () => {
-  try {
-    const { databases, account } = await createSessionClient();
-    const user = await account.get();
+  const { databases, account } = await createSessionClient();
+  const user = await account.get();
 
-    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
-      Query.equal('userId', user.$id),
-    ]);
+  const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+    Query.equal('userId', user.$id),
+  ]);
 
-    if (members.total === 0) {
-      return { documents: [], total: 0 };
-    }
-    const workspaceIds = members.documents.map((member) => member.workspaceId);
-
-    const workspaces = await databases.listDocuments(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      [Query.orderDesc('$createdAt'), Query.contains('$id', workspaceIds)]
-    );
-
-    return workspaces;
-    //workspaces looks like:
-    // {
-    //   documents: [
-    //     {
-    //       $id: '1',
-    //       $createdAt: '2021-01-01',
-    //       $updatedAt: '2021-01-01',
-  } catch {
+  if (members.total === 0) {
     return { documents: [], total: 0 };
   }
+  const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+  const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACES_ID, [
+    Query.orderDesc('$createdAt'),
+    Query.contains('$id', workspaceIds),
+  ]);
+
+  return workspaces;
+  //workspaces looks like:
+  // {
+  //   documents: [
+  //     {
+  //       $id: '1',
+  //       $createdAt: '2021-01-01',
+  //       $updatedAt: '2021-01-01',
 };
 
 interface GetWorkspaceProps {
   workspaceId: string;
 }
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
-  try {
-    const { databases, account } = await createSessionClient();
-    const user = await account.get();
+  const { databases, account } = await createSessionClient();
+  const user = await account.get();
 
-    //check if the user is a member of the workspace
-    const member = await getMember({
-      databases,
-      userId: user.$id,
-      workspaceId,
-    });
-    if (!member) return null;
+  //check if the user is a member of the workspace
+  const member = await getMember({
+    databases,
+    userId: user.$id,
+    workspaceId,
+  });
+  if (!member) throw new Error ('Unauthorized')
 
-    //type as Workspace to get type safety - otherwise it would be the generic document type from appwrite
-    const workspace = await databases.getDocument<Workspace>(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      workspaceId
-    );
+  //type as Workspace to get type safety - otherwise it would be the generic document type from appwrite
+  const workspace = await databases.getDocument<Workspace>(
+    DATABASE_ID,
+    WORKSPACES_ID,
+    workspaceId
+  );
 
-    return workspace;
-  } catch {
-    return null;
-  }
+  return workspace;
 };
 
 interface GetWorkspaceInfoProps {
@@ -72,20 +63,16 @@ interface GetWorkspaceInfoProps {
 export const getWorkspaceInfo = async ({
   workspaceId,
 }: GetWorkspaceInfoProps) => {
-  try {
-    const { databases } = await createSessionClient();
+  const { databases } = await createSessionClient();
 
-    //type as Workspace to get type safety - otherwise it would be the generic document type from appwrite
-    const workspace = await databases.getDocument<Workspace>(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      workspaceId
-    );
+  //type as Workspace to get type safety - otherwise it would be the generic document type from appwrite
+  const workspace = await databases.getDocument<Workspace>(
+    DATABASE_ID,
+    WORKSPACES_ID,
+    workspaceId
+  );
 
-    return {
-      name: workspace.name,
-    };
-  } catch {
-    return null;
-  }
+  return {
+    name: workspace.name,
+  };
 };
