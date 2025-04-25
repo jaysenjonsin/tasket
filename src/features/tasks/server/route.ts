f5ba8728a78b44f49179fb4dcec008e8e19e994c
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-04-24 20:08:03 -0700

    add sorting and pagination to data table

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -11,7 +11,7 @@ import {
 } from '../../../config';
 import { ID, Query } from 'node-appwrite';
 import { z } from 'zod';
-import { TaskStatus } from '../types';
+import { Task, TaskStatus } from '../types';
 import { createAdminClient } from '../../../lib/appwrite';
 import { Project } from '../../projects/types';
 const app = new Hono()
@@ -73,7 +73,11 @@ const app = new Hono()
         query.push(Query.search('name', search));
       }
 
-      const tasks = await databases.listDocuments(DATABASE_ID, TASKS_ID, query);
+      const tasks = await databases.listDocuments<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        query
+      );
 
       const projectIds = tasks.documents.map((task) => task.projectId);
       const assigneeIds = tasks.documents.map((task) => task.assigneeId);
