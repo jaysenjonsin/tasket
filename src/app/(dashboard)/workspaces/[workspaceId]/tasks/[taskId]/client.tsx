448ea5f92331b51ddc864669e3a68b568518093d
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-04-28 21:46:34 -0700

    add delete functionality to task-breadcrumbs

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -3,6 +3,7 @@ import { useTaskId } from '@/features/tasks/hooks/use-task-id';
 import { useGetTask } from '@/features/tasks/api/use-get-task';
 import { PageError } from '@/components/page-error';
 import { PageLoader } from '@/components/page-loader';
+import { TaskBreadcrumbs } from '@/features/tasks/components/task-breadcrumbs';
 export const TaskIdClient = () => {
   const taskId = useTaskId();
   const { data, isLoading } = useGetTask({ taskId });
@@ -13,5 +14,9 @@ export const TaskIdClient = () => {
   if (!data) {
     return <PageError message='Task not found' />;
   }
-  return <p>{JSON.stringify(data)}</p>;
+  return (
+    <div className='flex flex-col'>
+      <TaskBreadcrumbs project={data.project} task={data} />
+    </div>
+  );
 };
