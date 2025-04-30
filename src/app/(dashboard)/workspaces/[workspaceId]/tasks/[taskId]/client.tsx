8e5ce3c5fbee8cb761418021c1494a04bffca0e0
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-04-30 08:22:59 -0700

    add task description functionality

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -21,7 +21,7 @@ export const TaskIdClient = () => {
     <div className='flex flex-col'>
       <TaskBreadcrumbs project={data.project} task={data} />
       <DottedSeparator className='my-6' />
-      <div className='grid grid-cols-1 lg:grids-cols-2 gap-4'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
         <TaskOverview task={data} />
         <TaskDescription task={data} />
       </div>
