67330ce7c316d9b7ff7134e1f444c8338e92acd7
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-04-30 14:39:15 -0700

    implement analytics cards in workspace

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -9,14 +9,23 @@ import { useProjectId } from '@/features/projects/hooks/use-project-id';
 import { useGetProject } from '@/features/projects/api/use-get-project';
 import { PageError } from '@/components/page-error';
 import { PageLoader } from '@/components/page-loader';
+import { useGetProjectAnalytics } from '@/features/projects/api/use-get-project-analytics';
+import { Analytics } from '@/components/analytics';
 
 export const ProjectIdClient = () => {
   const projectId = useProjectId();
-  const { data, isLoading } = useGetProject({ projectId });
+  const { data: project, isLoading: isLoadingProject } = useGetProject({
+    projectId,
+  });
+  const { data: analytics, isLoading: isLoadingAnalytics } =
+    useGetProjectAnalytics({ projectId });
+
+  const isLoading = isLoadingProject || isLoadingAnalytics;
+
   if (isLoading) {
     return <PageLoader />;
   }
-  if (!data) {
+  if (!project) {
     return <PageError message='Project not found' />;
   }
   return (
@@ -24,16 +33,16 @@ export const ProjectIdClient = () => {
       <div className='flex items-center justify-between'>
         <div className='flex items-center gap-x-2'>
           <ProjectAvatar
-            name={data.name}
-            image={data.imageUrl}
+            name={project.name}
+            image={project.imageUrl}
             className='size-8'
           />
-          <p className='text-lg font-semi-bold'>{data.name}</p>
+          <p className='text-lg font-semi-bold'>{project.name}</p>
         </div>
         <div>
           <Button variant='secondary' size='sm' asChild>
             <Link
-              href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}
+              href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}
             >
               <PencilIcon className='size-4 mr-2' />
               Edit project
@@ -41,6 +50,7 @@ export const ProjectIdClient = () => {
           </Button>
         </div>
       </div>
+      {analytics ? <Analytics data={analytics} /> : null}
       <TaskViewSwitcher hideProjectFilter />
     </div>
   );
