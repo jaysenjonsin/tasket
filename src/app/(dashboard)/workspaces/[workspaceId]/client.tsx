44d154faa0a22f1a9071d8025b4a4fb08fcf0213
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-05-01 12:58:15 -0700

    downgrade date-fns

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -168,7 +168,7 @@ export const MembersList = ({ data, total }: MembersListProps) => {
     <div className='flex flex-col gap-y-4 col-span-1'>
       <div className='bg-white border rounded-lg p-4'>
         <div className='flex items-center justify-between'>
-          <p className='text-lg font-semibold'>Memberss ({total})</p>
+          <p className='text-lg font-semibold'>Members ({total})</p>
           <Button variant='secondary' size='icon' asChild>
             <Link href={`/workspaces/${workspaceId}/members`}>
               <SettingsIcon className='size-4 text-neutral-400' />
