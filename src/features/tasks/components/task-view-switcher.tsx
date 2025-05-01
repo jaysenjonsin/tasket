e15b4db3e18b749bfba0a917e3512a381338bade
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-05-01 09:10:29 -0700

    fix:
    add project filter to task view switcher

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -16,7 +16,7 @@ import { TaskStatus } from '../types';
 import { useCallback } from 'react';
 import { useBulkUpdateTasks } from '../api/use-bulk-update-tasks';
 import { DataCalendar } from './data-calendar';
-
+import { useProjectId } from '@/features/projects/hooks/use-project-id';
 interface TaskViewSwitcherProps {
   hideProjectFilter?: boolean;
 }
@@ -28,13 +28,14 @@ export const TaskViewSwitcher = ({
     defaultValue: 'table',
   });
   const workspaceId = useWorkspaceId();
+  const paramProjectId = useProjectId();
   const { open } = useCreateTaskModal();
 
   const { mutate: bulkUpdate } = useBulkUpdateTasks();
 
   const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
     workspaceId,
-    projectId,
+    projectId: paramProjectId || projectId,
     assigneeId,
     status,
     dueDate,
